8178edfdf55a7aaf6a857b367dada1ac85e56c0c
Daniel Thom <daniel.thom@nrel.gov>
2023-01-24 08:49:09 -0700

    Handle test output files dynamically

--- worker/tests/preprocess.py
+++ worker/tests/preprocess.py
@@ -4,15 +4,14 @@ import json
 import sys
 from pathlib import Path
 
-# python preprocess.py -i {inputs} -o {f1}
-if len(sys.argv) != 5:
-    raise Exception(f"bad inputs in work.py: {sys.argv}")
+# python preprocess.py -i {inputs} f1 f2
+if len(sys.argv) < 4:
+    raise Exception(f"bad inputs in preprocess.py: {sys.argv}")
 
 input_file = Path(sys.argv[2])
 if not input_file.exists():
     raise Exception(f"{input_file} does not exist")
 
-print(f"running {sys.argv}")
-output_files = [Path(sys.argv[4])]
+output_files = [Path(x) for x in sys.argv[3:]]
 for f in output_files:
     f.write_text(json.dumps({"hello": "world"}, indent=2) + "\n")
