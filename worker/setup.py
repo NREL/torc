8aa671b17f55c06d6b88269c9fcff282f401c5dd
Daniel Thom <daniel.thom@nrel.gov>
2023-02-10 12:22:12 -0700

    Add missing packages

--- worker/setup.py
+++ worker/setup.py
@@ -57,5 +57,5 @@ setup(
     extras_require={
         "dev": ["black", "flake8", "pytest"],
     },
-    install_requires=["click", "psutil", "prettytable", "s3path"],
+    install_requires=["click", "json5", "psutil", "prettytable", "pydantic", "s3path"],
 )
