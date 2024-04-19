a8cf720ade9bea30326d36219b269dc7bb89935c
Daniel Thom <daniel.thom@nrel.gov>
2024-04-19 11:57:05 -0600

    Bump version to v0.3.6

--- julia/Torc/src/api/APIClient.jl
+++ julia/Torc/src/api/APIClient.jl
@@ -7,7 +7,7 @@ using Dates, TimeZones
 using OpenAPI
 using OpenAPI.Clients
 
-const API_VERSION = "v0.3.5"
+const API_VERSION = "v0.3.6"
 
 include("modelincludes.jl")
 
--- torc_package/torc/__init__.py
+++ torc_package/torc/__init__.py
@@ -3,7 +3,7 @@
 import logging
 import warnings
 
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 warnings.filterwarnings("once", category=DeprecationWarning)
