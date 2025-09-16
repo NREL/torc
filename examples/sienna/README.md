58e5a49e8438a90376baae2b3578f34a584416da
Daniel Thom <daniel.thom@nrel.gov>
2025-09-16 09:37:43 -0600

    Add Sienna simulation example scripts

---
+++ examples/sienna/README.md
@@ -0,0 +1,58 @@
+# Usage
+The scripts in this directory demonstrate how to run a partitioned Sienna\Ops simulation through
+torc.
+
+## Sienna simulation script
+Create a simulation script with build and execute functions as defined in the
+  [Sienna docs](https://nrel-sienna.github.io/PowerSimulations.jl/latest/modeler_guide/parallel_simulations/#Run-a-Simulation-in-Parallel-on-an-HPC). Refer to the example script `run_rts_uc_ed.jl`.
+
+## Torc configuration script
+The script `configure_parallel_simulation.jl` builds a torc workflow and adds it to the database.
+It configures parameters for NREL's Kestrel HPC.
+
+Configuration parameters to customize:
+- TORC_SERVICE_URL: Adjust this for your environment.
+- Sienna simulation script: At the bottom of the file there is a call to `configure_parallel_simulation`.
+  The first parameter must be your Sienna simulation script. Configure the parameters in this
+  function call as desired.
+- Slurm account: Customize the instances of `SlurmSchedulerModel`. The example uses the debug
+  partition to run the build job and the short partition for the work jobs.
+- Resource requirements: Configure the instances of `ResourceRequirementsModel` for your jobs.
+  This will determine how many jobs torc runs in parallel on each node. Memory is usually the limiting
+  factor. For example, if each partitioned job needs 50 GB of memory, torc will will run 4 jobs in
+  parallel on each Kestrel standard node (which has ~250 GB - overhead).
+- Customize the `ComputeNodeScheduleParams.num_jobs` parameter. This tells torc how many compute nodes
+  to schedule for the work jobs. Torc will schedule them after the build job completes.
+- julia_env.sh: Edit this file if you need to perform different steps to make the julia binary available
+  in the system path. You may not need the call to `module load julia` if you have configured julia with
+  juliaup in your own environment.
+
+## Build the workflow
+```console
+$ julia --project <your-env-path>
+julia> include("configure_parallel_simulation.jl")
+julia> configure_parallel_simulation(
+    "run_RTS_UC-ED.jl",
+    365,
+    7,
+    "simulation_output";
+    num_overlap_steps=1,
+    project_path=".",
+    simulation_name="rts",
+)
+```
+The torc workflow key will be printed to the console. Something like
+```
+Created Torc workflow key = 61160926
+```
+
+## Start the workflow and schedule nodes
+```
+$ torc -k 61160926 workflows start
+```
+
+This command will prompt you to choose a Slurm config. Select the config that will run
+the build job. It will schedule the work jobs when complete, as discussed above.
+```
+$ torc -k 61160926 hpc slurm schedule-nodes -n1
+```
--- examples/sienna/configure_parallel_simulation.jl
+++ examples/sienna/configure_parallel_simulation.jl
@@ -1,18 +1,17 @@
-using PowerSimulations
+const TORC_SERVICE_URL = "http://dsgrid-registry.hpc.nrel.gov:8529/_db/sienna/torc-service"
 
+using PowerSimulations
 using Torc
 import Torc: APIClient
-
-const TORC_SERVICE_URL = "http://dsgrid-registry.hpc.nrel.gov:8529/_db/workflows/torc-service"
 
 function configure_parallel_simulation(
     script::AbstractString,
     num_steps::Integer,
     num_period_steps::Integer,
     output_dir::AbstractString;
-    num_overlap_steps::Integer=0,
-    project_path=nothing,
-    simulation_name="simulation",
+    num_overlap_steps::Integer = 1,
+    project_path = ".",
+    simulation_name = "simulation",
 )
     api = make_api(TORC_SERVICE_URL)
     workflow = create_workflow(api)
@@ -23,7 +22,7 @@ function configure_parallel_simulation(
             script,
             num_steps,
             num_period_steps,
-            output_dir,
+            output_dir;
             num_overlap_steps = num_overlap_steps,
             project_path = project_path,
             simulation_name = simulation_name,
@@ -38,10 +37,10 @@ function create_workflow(api)
     return send_api_command(
         api,
         APIClient.add_workflow,
-        APIClient.WorkflowModel(
+        APIClient.WorkflowModel(;
             user = get_user(),
-            name="partitioned_simulation",
-            description="Run a partitioned simulation.",
+            name = "rts_uc_ed_partitioned_simulation",
+            description = "Run an RTS UC-ED partitioned simulation.",
         ),
     )
 end
@@ -53,61 +52,91 @@ function build_workflow(
     num_steps::Integer,
     num_period_steps::Integer,
     output_dir::AbstractString;
-    num_overlap_steps::Integer=0,
-    project_path=nothing,
-    simulation_name="simulation",
+    num_overlap_steps::Integer = 0,
+    project_path = nothing,
+    simulation_name = "simulation",
 )
+    config = send_api_command(
+        api,
+        APIClient.get_workflow_config,
+        workflow._key,
+    )
+    config.compute_node_resource_stats = APIClient.ComputeNodeResourceStatsModel(;
+        cpu = true,
+        memory = true,
+        process = true,
+        interval = 5,
+        monitor_type = "periodic",
+        make_plots = true,
+    )
+    send_api_command(
+        api,
+        APIClient.modify_workflow_config,
+        workflow._key,
+        config,
+    )
+
     mkpath(output_dir)
     partitions = SimulationPartitions(num_steps, num_period_steps, num_overlap_steps)
     julia_cmd = isnothing(project_path) ? "julia" : "julia --project=$project_path"
-    setup_command = "$julia_cmd $script setup --simulation-name=$simulation_name " *
-    "--num-steps=$num_steps --num-period-steps=$num_period_steps " *
-    "--num-overlap-steps=$num_overlap_steps --output-dir=$output_dir"
+    setup_command =
+        "$julia_cmd $script setup --simulation-name=$simulation_name " *
+        "--num-steps=$num_steps --num-period-steps=$num_period_steps " *
+        "--num-overlap-steps=$num_overlap_steps --output-dir=$output_dir"
     teardown_command = "$julia_cmd $script join --simulation-name=$simulation_name --output-dir=$output_dir"
 
+    f1 = send_api_command(
+        api,
+        APIClient.add_file,
+        workflow._key,
+        APIClient.FileModel(;
+            name = "run_script",
+            path = "small/run_RTS_UC-ED.jl",
+        ),
+    )
     small = send_api_command(
         api,
         APIClient.add_resource_requirements,
         workflow._key,
-        APIClient.ResourceRequirementsModel(
-            name="small",
-            num_cpus=1,
-            memory="10g",
-            runtime="P0DT30M",
+        APIClient.ResourceRequirementsModel(;
+            name = "small",
+            num_cpus = 1,
+            memory = "10g",
+            runtime = "P0DT30M",
         ),
     )
     medium = send_api_command(
         api,
         APIClient.add_resource_requirements,
         workflow._key,
-        APIClient.ResourceRequirementsModel(
-            name="medium",
-            num_cpus=1,
-            memory="40g",
-            runtime="P0DT2H",
+        APIClient.ResourceRequirementsModel(;
+            name = "medium",
+            num_cpus = 1,
+            memory = "3g",
+            runtime = "P0DT10M",
         ),
     )
     send_api_command(
         api,
         APIClient.add_slurm_scheduler,
         workflow._key,
-        APIClient.SlurmSchedulerModel(
-            name="debug",
-            account="my_account",
-            nodes=1,
-            walltime="01:00:00",
-            partition="debug",
+        APIClient.SlurmSchedulerModel(;
+            name = "debug",
+            account = "siipspia",
+            nodes = 1,
+            walltime = "01:00:00",
+            partition = "debug",
         ),
     )
     short = send_api_command(
         api,
         APIClient.add_slurm_scheduler,
         workflow._key,
-        APIClient.SlurmSchedulerModel(
-            name="short",
-            account="my_account",
-            nodes=1,
-            walltime="04:00:00",
+        APIClient.SlurmSchedulerModel(;
+            name = "short",
+            account = "siipspia",
+            nodes = 1,
+            walltime = "04:00:00",
         ),
     )
 
@@ -115,46 +144,61 @@ function build_workflow(
         api,
         APIClient.add_job,
         workflow._key,
-        APIClient.JobModel(
-            name="setup",
-            command=setup_command,
-            resource_requirements=small._id,
-            invocation_script="bash julia_env.sh",
-       ))
+        APIClient.JobModel(;
+            name = "build",
+            command = setup_command,
+            resource_requirements = small._id,
+            invocation_script = "bash julia_env.sh",
+            input_files = [f1._id],
+        ))
 
     work_jobs = String[]
     for i in 1:get_num_partitions(partitions)
         cmd = "$julia_cmd $script execute --simulation-name=$simulation_name --index=$i --output-dir=$output_dir"
-        job = APIClient.JobModel(
-            name="execute-$i",
-            command=cmd,
-            resource_requirements=medium._id,
-            blocked_by=[setup._id],
-            invocation_script="bash julia_env.sh",
-       )
+        job = APIClient.JobModel(;
+            name = "execute-$i",
+            command = cmd,
+            resource_requirements = medium._id,
+            blocked_by = [setup._id],
+            cancel_on_blocking_job_failure = true,
+            invocation_script = "bash julia_env.sh",
+        )
         if i == 1
             # Only one job needs to ask for scheduling.
             job.schedule_compute_nodes=APIClient.ComputeNodeScheduleParams(
-                num_jobs=20,
+                num_jobs=5,
                 scheduler_id=short._id,
             )
         end
         job = send_api_command(api, APIClient.add_job, workflow._key, job)
-            push!(work_jobs, job._id)
+        push!(work_jobs, job._id)
     end
 
     send_api_command(
         api,
         APIClient.add_job,
         workflow._key,
-        APIClient.JobModel(
-            name="teardown",
-            command=teardown_command,
-            resource_requirements=small._id,
-            blocked_by=work_jobs,
-            invocation_script="bash julia_env.sh",
-       ),
+        APIClient.JobModel(;
+            name = "join",
+            command = teardown_command,
+            resource_requirements = small._id,
+            blocked_by = work_jobs,
+            invocation_script = "bash julia_env.sh",
+            cancel_on_blocking_job_failure = true,
+        ),
     )
+
+    # TODO: add job for results processing.
 
     println("Created Torc workflow key = $(workflow._key)")
 end
+
+configure_parallel_simulation(
+    "run_rts_uc_ed.jl",
+    365,
+    7,
+    "simulation_output";
+    num_overlap_steps = 1,
+    project_path = ".",
+    simulation_name = "rts",
+)
